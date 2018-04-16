0deb25538ed175dc6212e31108d29098f1e4e329
Rob Loach <robloach@gmail.com>
2018-04-16 08:59:56 -0400

    Update dependencies

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const mjml = require('mjml')
+const extend = require('extend-shallow')
 
 exports.name = 'mjml'
 exports.outputFormat = 'html'
 
-exports.render = function (str) {
-  const result = mjml.mjml2html(str)
+exports.render = function (str, options, locals) {
+  const opts = extend({}, options, locals)
+  const result = mjml(str, opts)
   return result.html
 }
